ce9dbce1b4118a24d10a847353989669fe106508
DSett-codes <settdebmalya273@gmail.com>
2026-02-20 16:08:23 +0530

    wrote the modeling scripts

--- src/modeling/predict.py
+++ src/modeling/predict.py
@@ -3,27 +3,55 @@ from pathlib import Path
 from loguru import logger
 from tqdm import tqdm
 import typer
-
+import sys
+import joblib
+import pandas as pd
+from sklearn.metrics import r2_score
 from src.config import MODELS_DIR, PROCESSED_DATA_DIR
 
 app = typer.Typer()
 
+TARGET = 'trip_duration'
+model_name = 'xgbreg.joblib'
+
+def load_dataframe(path):
+    df = pd.read_csv(path)
+    return df
+    
+    
+def make_X_y(dataframe:pd.DataFrame,target_column:str):
+    df_copy = dataframe.copy()
+    
+    X = df_copy.drop(columns=target_column)
+    y = df_copy[target_column]
+    
+    return X, y
 
 @app.command()
 def main(
     # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    features_path: Path = PROCESSED_DATA_DIR / "test_features.csv",
-    model_path: Path = MODELS_DIR / "model.pkl",
-    predictions_path: Path = PROCESSED_DATA_DIR / "test_predictions.csv",
+    data_path: Path = PROCESSED_DATA_DIR / 'final',
+    model_path: Path = MODELS_DIR / "models" / model_name,
+    predictions_path: Path = PROCESSED_DATA_DIR / 'test' ,
     # -----------------------------------------
 ):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Performing inference for model...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Inference complete.")
-    # -----------------------------------------
+    data = load_dataframe(data_path/'val.csv')
+    # split the data into X and y
+    X_test, y_test = make_X_y(dataframe=data,target_column=TARGET)
+    # load the model
+    model = joblib.load(model_path)
+    # get predictions from model
+    y_pred = model.predict(X_test)
+    # calcuate the r2 score
+    score = r2_score(y_test,y_pred)
+    
+    logger.info(f'The score for dataset {data_path.stem} is {score}')
+    test_data = load_dataframe(data_path/'test.csv')
+    test_pred = model.predict(test_data)
+    df = pd.DataFrame({TARGET: test_pred})
+    df.to_csv(predictions_path/'test_predictions.csv', index_label="index")
+    
+    logger.success(f'Test predictions has been saved to {predictions_path}')
 
 
 if __name__ == "__main__":
--- src/modeling/train.py
+++ src/modeling/train.py
@@ -1,4 +1,10 @@
 from pathlib import Path
+import joblib
+import sys
+import pandas as pd
+from yaml import safe_load
+from xgboost import XGBRegressor
+from sklearn.ensemble import RandomForestRegressor
 
 from loguru import logger
 from tqdm import tqdm
@@ -8,22 +14,58 @@ from src.config import MODELS_DIR, PROCESSED_DATA_DIR
 
 app = typer.Typer()
 
+TARGET = 'trip_duration'
 
+def load_dataframe(path):
+    df = pd.read_csv(path)
+    return df
+    
+    
+    
+def make_X_y(dataframe:pd.DataFrame,target_column:str):
+    df_copy = dataframe.copy()
+    
+    X = df_copy.drop(columns=target_column)
+    y = df_copy[target_column]
+    
+    return X, y
+
+
+def train_model(model,X_train,y_train):
+    # fit the model on data
+    model.fit(X_train,y_train)
+    logger.success('Model Training Complete')
+    
+    return model
+
+
+def save_model(model,save_path):
+    joblib.dump(value=model,
+                filename=save_path)
+    
+    
 @app.command()
 def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    features_path: Path = PROCESSED_DATA_DIR / "features.csv",
-    labels_path: Path = PROCESSED_DATA_DIR / "labels.csv",
-    model_path: Path = MODELS_DIR / "model.pkl",
-    # -----------------------------------------
+    training_data_path: Path= PROCESSED_DATA_DIR / 'final' / 'train.csv',
+    model_output_path: Path = MODELS_DIR / 'models'
 ):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Training some model...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Modeling training complete.")
-    # -----------------------------------------
+ 
+    train_data = load_dataframe(training_data_path)
+    # split the data into X and y
+    X_train, y_train = make_X_y(dataframe=train_data,target_column=TARGET)
+    # read the parameters from params.yaml
+    with open('params.yaml') as f:
+        params = safe_load(f)
+    # make the model object
+    regressor = XGBRegressor(**params['train_model']['xgb_regressor'])
+    # train the model
+    regressor = train_model(model=regressor,
+                            X_train=X_train,
+                            y_train=y_train)
+    # save the model after training
+   
+    model_output_path.mkdir(exist_ok=True)
+    save_model(model=regressor,save_path=model_output_path / 'xgbreg.joblib')
 
 
 if __name__ == "__main__":
